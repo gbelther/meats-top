f6be4d9a7d0cdb9c314d8e33ab099ec14dfe2c07
GBelther <belthergabriel@gmail.com>
2022-01-03 18:46:29 -0300

    feature: criação do componente header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <nav className="header__navigation">
+        <a className="header__navigation--item" href="#">
+          Home
+        </a>
+        <a className="header__navigation--item" href="#">
+          Sobre
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
